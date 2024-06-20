166db634745109903913c54d450147628b444f13
dafi <fauzandafi121@gmail.com>
2024-06-20 20:01:34 +0700

    semangat hehehe

--- dasboard/app.js
+++ dasboard/app.js
@@ -1,25 +1,15 @@
-const buyButton = document.getElementById("buy");
 const closeAlert = document.getElementById("ok");
-const buyBtn = document.getElementsByClassName("buy");
+const buyBtn = document.querySelectorAll(".buy");
 
-// for (var i = 0; i < buyButton; i++) {
-//   const buyButtons = buyButton[i];
-//   buyButtons.addEventListener("click", () => {
-//     console.log("hai: " + i);
-//   });
-// }
-
-buyButton.addEventListener("click", function () {
-  console.log("haii");
-});
-
-for (let i = 0; i < buyBtn; i++) {
+for (var i = 0; i < buyBtn.length; i++) {
   const buyButtons = buyBtn[i];
-  buyButtons.addEventListener("click", function () {
-    console.log("sukses: " + i);
+  buyButtons.addEventListener("click", () => {
+    let alertBox = document.getElementById("box-alert");
+    setTimeout(function () {
+      alertBox.style.display = "flex";
+    }, 1000);
   });
 }
-
 closeAlert.addEventListener("click", function () {
   let alertBox = document.getElementById("box-alert");
   alertBox.style.display = "none";
